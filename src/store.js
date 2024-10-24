b623078ccdf6504922df51e020999ea10f5d21cd
Ziad Ahmed <ziad.a200343@gmail.com>
2024-10-24 18:28:28 +0300

    removed all garbage code

--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "./features/postSlice";
-import commentsReducer from "./features/commentsSlice";
 
 export const store = configureStore({
   reducer: {
     posts: postsReducer,
-    comments: commentsReducer,
   },
 });
